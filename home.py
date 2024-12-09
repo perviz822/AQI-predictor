efebe874cf7f95221cbad6a85da0eae4bb96968f
Parviz <perviz822@gmail.com>
2024-12-09 15:04:29 +0100

    modifief

--- home.py
+++ home.py
@@ -104,7 +104,7 @@ def display_aqi_prediction_page():
             xaxis_title='Date',
             yaxis_title='Air Quality Index',
             template='plotly_white',
-            height=450,
+            height=300,
             width=700,
             hovermode='closest',
         )
@@ -126,11 +126,11 @@ def display_aqi_prediction_page():
     fig.update_layout(title='Daily Average AQI for Baku', yaxis_title='Average AQI', title_x=0.5, template='plotly_white')
 
     with col1:
-        st.plotly_chart(fig,use_container_width=False)
+        st.plotly_chart(fig,use_container_width=True)
         loaded_df = pickle.load(open('predictions_for_three_days.pkl', 'rb'))
         clean_fig = create_clean_aqi_plot(daily_aqi, loaded_df)
        
     with col2:
             
-       st.plotly_chart(clean_fig,use_container_width=False)
+       st.plotly_chart(clean_fig,use_container_width=True)
        
